292c30361e95036a7690f33d209aa35970eae657
Aadi Bajpai <hello@aadibajpai.com>
2019-03-11 01:29:08 +0530

    update label and refactor JS
    Signed-off-by: Aadi Bajpai <aadibajpai@gmail.com>

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,20 +1,20 @@
 function toggle() {
-    var themeButton = document.getElementById("unchecked");
+    let themeButton = document.getElementById("unchecked");
+    let svg5 = document.getElementsByClassName('st5');
+    let svg3 = document.getElementsByClassName('st3');
+    let svg4 = document.getElementsByClassName('st4');
     if (themeButton.value === "Enable Light Mode"){
         themeButton.value = "Enable Dark Mode";
         document.body.style.backgroundColor = "#f5f5f5";
         document.getElementById("header").style.backgroundColor = "#f5f5f5";
         document.getElementById("spn").style.color = "rgba(33,33,33,0.8)";
-        var svg5 = document.getElementsByClassName('st5');
-        for (var i = 0; i < svg5.length; i++) {
+        for (let i = 0; i < svg5.length; i++) {
             svg5[i].style.fill = "rgba(0, 0, 0, 0.1)";
         }
-        var svg3 = document.getElementsByClassName('st3');
-        for (var i = 0; i < svg3.length; i++) {
+        for (let i = 0; i < svg3.length; i++) {
             svg3[i].style.fill = "#f5f5f5";
         }
-        var svg4 = document.getElementsByClassName('st4');
-        for (var i = 0; i < svg4.length; i++) {
+        for (let i = 0; i < svg4.length; i++) {
             svg4[i].style.fill = "#757575";
         }
     }
@@ -23,16 +23,13 @@ function toggle() {
         document.body.style.backgroundColor = "#202124";
         document.getElementById("header").style.backgroundColor = "#202124";
         document.getElementById("spn").style.color = "whitesmoke";
-        var svg5 = document.getElementsByClassName('st5');
-        for (var i = 0; i < svg5.length; i++) {
+        for (let i = 0; i < svg5.length; i++) {
             svg5[i].style.fill = "rgba(255, 255, 255, 0.1)";
         }
-        var svg3 = document.getElementsByClassName('st3');
-        for (var i = 0; i < svg3.length; i++) {
+        for (let i = 0; i < svg3.length; i++) {
             svg3[i].style.fill = "#212121";
         }
-        var svg4 = document.getElementsByClassName('st4');
-        for (var i = 0; i < svg4.length; i++) {
+        for (let i = 0; i < svg4.length; i++) {
             svg4[i].style.fill = "#dfe0e4";
         }
     }
