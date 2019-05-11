acb82f1c7eb1f376cbdfc1342b0cc4f8007a1a23
Aadi Bajpai <me@aadibajpai.me>
2019-05-12 01:55:31 +0530

    fix cookie
    Signed-off-by: Aadi Bajpai <me@aadibajpai.me>

--- scripts/chaffle.js
+++
@@ -1,8 +0,0 @@
-function shuffle() {
-    var elements = document.querySelectorAll('[data-chaffle]');
-    Array.prototype.forEach.call(elements, function (el) {
-        var chaffle = new Chaffle(el, { /* options */ });
-        chaffle.init();
-    });
-}
-window.onload = shuffle;
